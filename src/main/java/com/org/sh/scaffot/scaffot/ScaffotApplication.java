0c33dd365d88485de524175b84aa9c7fbf352222
romance <ymcdhl@163.com>
2020-04-22 20:58:49 +0800

    init scaffot

---
+++ src/main/java/com/org/sh/scaffot/scaffot/ScaffotApplication.java
@@ -0,0 +1,13 @@
+package com.org.sh.scaffot.scaffot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScaffotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScaffotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/org/sh/scaffot/scaffot/ScaffotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.sh.scaffot.scaffot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScaffotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
