52f4d37769caa291aaf5c8830502d8af74f97fec
romance <ymcdhl@163.com>
2020-04-22 23:34:47 +0800

    init scaffot

---
+++ src/main/java/com/org/sh/scaffot/scaffot/config/RedisConfig.java
@@ -0,0 +1,22 @@
+package com.org.sh.scaffot.scaffot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate getRedisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        return redisTemplate;
+    }
+
+
+}
---
+++ src/main/java/com/org/sh/scaffot/scaffot/controller/HelloWorldController.java
@@ -0,0 +1,34 @@
+package com.org.sh.scaffot.scaffot.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("hello")
+public class HelloWorldController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+
+    @GetMapping
+    public String helloWorld(){
+
+        redisTemplate.opsForValue().set("testRedis","成功了");
+        Boolean testRedis = redisTemplate.hasKey("testRedis");
+        System.out.println(testRedis);
+
+        Object testRedis1 = redisTemplate.opsForValue().get("testRedis");
+
+        System.out.println(testRedis1);
+
+
+        return "hello world";
+    }
+
+}
