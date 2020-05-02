5c9089e023a6722e4fb9db62995b02530a4344c1
sh <ymcdhl@163.com>
2020-05-02 11:16:24 +0800

    测试redis成功

---
+++ scaffot-common/src/main/java/com/org/sh/scaffot/common/util/HttpPool.java
@@ -0,0 +1,15 @@
+package com.org.sh.scaffot.common.util;
+
+import java.net.HttpURLConnection;
+
+public class HttpPool {
+
+
+    public static void main(String[] args) {
+
+        HttpURLConnection httpURLConnection ;
+
+    }
+
+
+}
